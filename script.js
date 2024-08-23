90e34a837045aad129f4aefe21f68b345d99804d
Brittanie Morgan <brittanie.morgan@mymona.uwi.edu>
2024-08-23 08:50:34 -0500

    updated card font size and responsiveness

--- script.js
+++ script.js
@@ -13,29 +13,3 @@ window.onscroll = function (e)
         navbar.style.opacity = 0
     }
 }
-
-/*
-document.addEventListener('DOMContentLoaded', (event) => {
-    const toggleSwitch = document.getElementById('theme-toggle');
-
-    // Check the saved theme from localStorage
-    const currentTheme = localStorage.getItem('theme');
-    if (currentTheme) {
-        document.documentElement.setAttribute('data-theme', currentTheme);
-        if (currentTheme === 'dark') {
-            toggleSwitch.checked = true;
-        }
-    }
-
-    // Event listener for the toggle switch
-    toggleSwitch.addEventListener('change', () => {
-        if (toggleSwitch.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }
-    });
-});
-*/
