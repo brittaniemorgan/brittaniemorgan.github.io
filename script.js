4ee15e5760df7b13cf6459bfda3b494b6b67dac7
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2024-09-27 14:47:37 -0500

    added dark mode

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-window.onload = () => {    
-    AOS.init();    
+window.onload = () => {
+    AOS.init();
+    const toggle = document.getElementById('themeToggle');
+
+    toggle.addEventListener('change', () => {
+      if (toggle.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+      }
+    });
+
 }
 
-window.onscroll = function (e)
-{
+window.onscroll = function (e) {
     var navbar = document.getElementsByTagName('nav')[0];
     var header = document.getElementById('landing');
 
-    if (window.scrollY >= header.offsetHeight/2) {
+    if (window.scrollY >= header.offsetHeight / 2) {
         navbar.style.opacity = 1
     } else {
         navbar.style.opacity = 0
     }
 }
+
